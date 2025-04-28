0e8e41bab3470f92c9918ab0d7b25216b0c9be5d
Jojo823-svg <joy.mutinda@strathmore.edu>
2025-04-28 11:48:18 +0300

    made a basic java code

---
+++ src/Courses.java
@@ -0,0 +1,9 @@
+//Basic java code
+public class Courses {
+    void course(){
+        System.out.println("Computer science");
+    }
+public static void main(String[] args) {
+    (new Courses()).course();
+}
+}
