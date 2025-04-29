a5ae1eeb8e53c508848fceee555b89be486d4e12
Jojo823-svg <joy.mutinda@strathmore.edu>
2025-04-29 13:09:20 +0300

    made a decision code using if...else

---
+++ src/PINBooleanChallenge.java
@@ -0,0 +1,17 @@
+public class PINBooleanChallenge {
+    int correctPin= 2025;
+    int enteredPin=1988;
+
+    void checkPin() {
+        if (enteredPin == correctPin) {
+            System.out.println("Correct Pin.");
+        } else {
+            System.out.println("Incorrect Pin.");
+        }
+    }
+    public static void main(String[]args){
+            new PINBooleanChallenge().checkPin();
+
+        }
+    }
+
