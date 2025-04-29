7fea43503b5442d08da2fa47537e497457b3ee97
Jojo823-svg <joy.mutinda@strathmore.edu>
2025-04-29 14:29:02 +0300

    made a code using the the random with if...else for rolling a dice

---
+++ src/DiceRoll.java
@@ -0,0 +1,25 @@
+import java.util.Random;
+
+public class DiceRoll {
+    public static void main(String[] args) {
+
+        Random rand = new Random();
+
+
+        int diceRoll = rand.nextInt(6) + 1;
+
+        String rollMessage;
+
+        if (diceRoll == 1) {
+            rollMessage = "You rolled the lowest number.";
+        } else if (diceRoll == 6) {
+            rollMessage = "Congratulations! You rolled the highest number.";
+        } else {
+            rollMessage = "Nice roll!";
+        }
+
+        System.out.println("You rolled a: " + diceRoll);
+        System.out.println(rollMessage);
+    }
+}
+
